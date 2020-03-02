c46f2125c0e6eeea0c8d2df68fb64204293cf058
Sergei <rat87@mail.ru>
2020-03-03 02:41:55 +0300

    double balloons and codestyle fixed

--- src/index.js
+++ src/index.js
@@ -1,53 +1,57 @@
 ymaps.ready(init);
 
-
-let myBalloon = document.querySelector("#window_balloon"),
-    closeButton = document.querySelector("#button__close"),
-    addButton = document.querySelector("#button__add"),
-    address = document.querySelector("#address"),
-    inputName = document.querySelector("#input__name"),
-    inputPlace = document.querySelector("#input__place"),
-    comments = document.querySelector("#comments"),
-    inputText = document.querySelector("#input__text"),
-    map = document.querySelector("#map");
-    placemarks = [];
+let myBalloon = document.querySelector('#window_balloon'),
+    closeButton = document.querySelector('#button__close'),
+    addButton = document.querySelector('#button__add'),
+    address = document.querySelector('#address'),
+    inputName = document.querySelector('#input__name'),
+    inputPlace = document.querySelector('#input__place'),
+    comments = document.querySelector('#comments'),
+    inputText = document.querySelector('#input__text'),
+    map = document.querySelector('#map'),
+    placemarks = [],
+    allCarousel = document.querySelector('.ymaps-2-1-75-balloon__close'),
+    coords,
+    newPlacemark;
 
 function init() {
     let myPlacemark,
-        myMap = new ymaps.Map("map", {
+        myMap = new ymaps.Map('map', {
             center: [54.51, 36.26],
             zoom: 12
         });
 
     // Создание кластера.
     let clusterer = new ymaps.Clusterer({
-        preset: "islands#invertedNightClusterIcons",
+        preset: 'islands#invertedNightClusterIcons',
         groupByCoordinates: false,
         clusterDisableClickZoom: true,
         clusterHideIconOnBalloonOpen: false,
         geoObjectHideIconOnBalloonOpen: false,
         clusterOpenBalloonOnClick: true,
-        clusterBalloonContentLayout: "cluster#balloonCarousel",
+        clusterBalloonContentLayout: 'cluster#balloonCarousel',
         clusterBalloonPanelMaxMapArea: 0,
         clusterBalloonContentLayoutWidth: 200,
         clusterBalloonContentLayoutHeight: 150,
         clusterBalloonPagerSize: 10,
-        clusterBalloonPagerType: "marker",
+        clusterBalloonPagerType: 'marker',
     });
 
     clusterer.add(placemarks);
     myMap.geoObjects.add(clusterer);
 
     if (sessionStorage.data) {
-        console.log('+');
         dataObj = JSON.parse(sessionStorage.data);
-        console.log(dataObj);
 
         for (let i = 0; i < dataObj.length; i++) {
-            newPlacemark = new ymaps.Placemark(dataObj[i].coords, {
+            let newPlacemark = new ymaps.Placemark(dataObj[i].coords, {
                 balloonContentHeader: inputPlace.value,
-                balloonContentBody: `<a onclick="openBalloonFull()" class="balloon__address_link">${dataObj[i].address}</a>`,
+                balloonContentBody: `<a onclick='openBalloonFull()' class='balloon__address_link'>${dataObj[i].address}</a>`,
                 balloonContentFooter: dataObj[i].comments
+            }, {
+                //preset: 'islands#nightDotIcon',
+                draggable: false,
+                openBalloonOnClick: false // Используем custom balloon.
             });
             newPlacemark.commentContent = dataObj[i].comments;
             newPlacemark.place = dataObj[i].address;
@@ -59,26 +63,29 @@ function init() {
 
         myMap.geoObjects.events.add('click', e => {
             comments.innerHTML = '';
-            comments.innerHTML = e.get("target").commentContent;
-            address.innerText = e.get("target").place;
-            console.log(e.get("target"));
-            coords = e.get("target").geometry._coordinates;
-            //console.log(e.get("target").properties._data.geoObjects[0].commentContent)
-            //console.log(e.get("target").properties._data.geoObjects[0].place)
-            if (e.get("target").options._name !== "cluster") openBalloon();
-            else {
-                //e.get("target").properties._data.geoObjects[0].place = e.get("target").place;
-               // e.get("target").properties._data.geoObjects[0].commentContent = e.get("target").commentContent;
+            comments.innerHTML = e.get('target').commentContent;
+            address.innerText = e.get('target').place;
+            coords = e.get('target').geometry._coordinates;
+            openBalloon();
+
+            if (e.get('target').options._name == 'cluster') {
+                if (myBalloon.style.display == 'block') {
+                    myBalloon.style.display = 'none';
+                }
+            } else {
+                let allCarousel = document.querySelector('.ymaps-2-1-75-balloon__close');
+                if (allCarousel != null) {
+                    allCarousel.click();
+                }
             }
         });
     }
 
     // Слушаем клик на карте.
-    myMap.events.add("click", e => {
-        coords = e.get("coords");
-        comments.innerHTML = "Отзывов пока нет...";
+    myMap.events.add('click', e => {
+        coords = e.get('coords');
 
-        // Выводим окно с отзывами и формой.
+        comments.innerHTML = 'Отзывов пока нет...';
         openBalloon();
         myPlacemark = createPlacemark(coords);
         getAddress(coords);
@@ -113,7 +120,7 @@ function init() {
         });
     }
 
-    addButton.addEventListener("click", () => {
+    addButton.addEventListener('click', () => {
         if (inputName.value && inputPlace.value && inputText.value) {
             // Получаем адрес отзыва.
             let addressLink = address.innerText;
@@ -129,13 +136,12 @@ function init() {
                 currentTime = `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
 
             // Создаём метку.
-            let newPlacemark = new ymaps.Placemark(
+            newPlacemark = new ymaps.Placemark(
                 coords, {
                     balloonContentHeader: inputPlace.value,
-                    balloonContentBody: `<a onclick="openBalloonFull()" class="balloon__address_link">${addressLink}</a><br><br>${inputText.value}<br><br>`,
+                    balloonContentBody: `<a onclick='openBalloonFull()' class='balloon__address_link'>${addressLink}</a><br><br>${inputText.value}<br><br>`,
                     balloonContentFooter: currentTime
                 }, {
-                    //preset: "islands#nightDotIcon",
                     draggable: false,
                     openBalloonOnClick: false // Используем custom balloon.
                 }
@@ -146,21 +152,17 @@ function init() {
             clusterer.add(newPlacemark);
             placemarks.push(newPlacemark);
 
-            
-
-
             // Обновляем содержимое нашего балуна
-            if (comments.innerHTML === "Отзывов пока нет...") comments.innerHTML = "";
+            if (comments.innerHTML === 'Отзывов пока нет...') comments.innerHTML = '';
             newPlacemark.commentContent =
                 `<div><span><b>${inputName.value}</b></span>
-                 <span class="ligth">${inputPlace.value}</span>
-                <span class="ligth">${currentTime}:</span><br>
+                 <span class='ligth'>${inputPlace.value}</span>
+                <span class='ligth'>${currentTime}:</span><br>
                  <span>${inputText.value}</span></div><br>`;
             comments.innerHTML += newPlacemark.commentContent;
             newPlacemark.place = address.innerText;
 
-            // debugger;
-            var sData = sessionStorage.data ? JSON.parse(sessionStorage.data) : [];
+            let sData = sessionStorage.data ? JSON.parse(sessionStorage.data) : [];
             let obj = {
                 comments: newPlacemark.commentContent,
                 address: newPlacemark.place || '',
@@ -168,82 +170,64 @@ function init() {
             };
             sData.push(obj);
             sessionStorage.data = JSON.stringify(sData);
-
-            // Очищаем инпуты.
             clearInputs();
 
-            newPlacemark.events.add("click", () => {
+            newPlacemark.events.add('click', () => {
                 openBalloon();
                 comments.innerHTML = newPlacemark.commentContent;
                 address.innerText = newPlacemark.place;
             });
         } else {
-            alert("Необходимо заполнить все поля!");
+            alert('Необходимо заполнить все поля!');
         }
     });
 }
 
-closeButton.addEventListener("click", () => {
-    myBalloon.style.display = "none";
+closeButton.addEventListener('click', () => {
+    myBalloon.style.display = 'none';
     clearInputs();
 });
 
 function clearInputs() {
-    inputName.value = "";
-    inputPlace.value = "";
-    inputText.value = "";
+    inputName.value = '';
+    inputPlace.value = '';
+    inputText.value = '';
 }
 
 // Наш кастомный балун.
 function openBalloon() {
-    myBalloon.style.top = event.clientY + "px";
-    myBalloon.style.left = event.clientX + "px";
-    myBalloon.style.display = "block";
-
-    console.log(myBalloon.clientWidth, myBalloon.clientHeight);
-    console.log(document.body.clientWidth, map.clientHeight)
+    myBalloon.style.top = event.clientY + 'px';
+    myBalloon.style.left = event.clientX + 'px';
+    myBalloon.style.display = 'block';
 
     let wH = map.clientHeight,
-    wW = map.clientWidth,
-    hD = myBalloon.clientHeight + Number.parseInt(myBalloon.style.top),
-    wD = myBalloon.clientWidth + Number.parseInt(myBalloon.style.left),
-    deltaH = wH - hD - 10,
-    deltaW = wW - wD - 10;
+        wW = map.clientWidth,
+        hD = myBalloon.clientHeight + Number.parseInt(myBalloon.style.top),
+        wD = myBalloon.clientWidth + Number.parseInt(myBalloon.style.left),
+        deltaH = wH - hD - 10,
+        deltaW = wW - wD - 10;
 
-    if (deltaH > 0) {
-        console.log('Hбольше', deltaH);
-    } else {
-        console.log('Hменьше', deltaH);
-        myBalloon.style.top = Number.parseInt(myBalloon.style.top) + deltaH + "px";
+    if (deltaH < 0) myBalloon.style.top = Number.parseInt(myBalloon.style.top) + deltaH + 'px';
+    if (deltaW < 0) myBalloon.style.left = Number.parseInt(myBalloon.style.left) + deltaW + 'px';
+
+    let allCarousel = document.querySelector('.ymaps-2-1-75-balloon__close');
+    if (allCarousel != null) {
+        allCarousel.click();
     }
-
-    if (deltaW > 0) {
-        console.log('Wбольше', deltaW);
-    } else {
-        console.log('Wменьше', deltaW);
-        myBalloon.style.left = Number.parseInt(myBalloon.style.left) + deltaW + "px";
-    }
-
-    console.log(myBalloon);
 }
 
 // Балун с контентом из placemarks.
 function openBalloonFull() {
-    address.innerText = "";
-    comments.innerHTML = "";
-    addressLink = document.querySelector(".balloon__address_link");
-    let allCarousel = document.querySelector(".ymaps-2-1-75-balloon__close-button");
+    address.innerText = '';
+    comments.innerHTML = '';
+    addressLink = document.querySelector('.balloon__address_link');
 
 
     for (let i = 0; i < placemarks.length; i++) {
         if (addressLink.innerText === placemarks[i].place) {
             address.innerText = placemarks[i].place;
             comments.innerHTML += placemarks[i].commentContent;
-            console.log("Мы тут были");
-        } else {
-            console.log("Какая-то лажа");
         }
     }
     openBalloon();
-    allCarousel.click();
 }
